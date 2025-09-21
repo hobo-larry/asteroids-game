38bb82b8531b2707cf9f6ff96a80f12d90bb11fc
Hobo-Larry <wrgamejuter@gmail.com>
2025-09-21 12:57:11 +0100

    removed the unnecessary prints

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 def main():
     pygame.get_init()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
