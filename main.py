1a4cc169073b737cdf5bb074778ba8ba3c7e1d11
Hobo-Larry <wrgamejuter@gmail.com>
2025-09-21 13:52:40 +0100

    add player and draw it to screen

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_RADIUS
+from player import Player
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 def main():
     pygame.get_init()
     clock = pygame.time.Clock()
     dt = 0
+   
+    player = Player( x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000 
+        
  
     
 
